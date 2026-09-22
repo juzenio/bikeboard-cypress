0e0c2fcfbd534b776276bdfbd222049e23d62ed0
Juzenio <juzenio.santos01@gmail.com>
2026-09-22 16:41:17 -0300

    incluindo relatorio mocha para pipiline ajuste 5

--- cypress/e2e/ads.cy.js
+++ cypress/e2e/ads.cy.js
@@ -20,7 +20,7 @@ describe('Cadastro de Anúncio', () => {
       .should('be.visible')
 
     cy.contains('Seu anúncio foi enviado para nossa equipe de moderação')
-      .should('be.visible')
+      .should('visible')
   })
 
   it('deve exibir erros para todos os campos obrigatórios não preenchidos', () => {
